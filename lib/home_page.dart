55d6c14a6c0663dc6bcb2349facd52dacd6d068b
aniketjain8441 <aniketjain8441@gmail.com>
2022-08-20 00:25:59 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import "package:flutter/material.dart";
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var age = 22;
+    var name = "Aniket Jain";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+          child: Container(child: Text("My name is $name.\nMy age is $age."))),
+      drawer: Drawer(),
+    );
+  }
+}
